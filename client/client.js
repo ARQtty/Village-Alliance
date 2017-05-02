18569532fac7cc96cabc37b3c5626a6a178b7646
ARQtty <mr.bookmix@gmail.com>
2017-05-03 01:21:05 +0300

    Add cell's value define function

--- client/client.js
+++ client/client.js
@@ -62,3 +62,24 @@ function mvViewportL() {
 	}
 	window.app.graphics.fillMap();
 }
+
+window.onload = function() {
+
+	function getCellByPosition(top, left) {
+		var topIndex = Math.floor(top / window.app.graphics.cellSize)
+		var leftIndex = Math.floor(left / window.app.graphics.cellSize)
+		
+		var leftIndexStr = '['+leftIndex.toString()+']';
+		var topIndexStr = '['+topIndex.toString()+']';
+		console.log('cells'+topIndexStr+leftIndexStr+' value='+window.app.graphics.cells[topIndex][leftIndex])
+
+		return window.app.graphics.cells[topIndex][leftIndex]
+	}
+
+
+	function handleMouseDown(event) {
+		filling = getCellByPosition(event.layerX, event.layerY);
+	}
+
+	window.app.graphics.canvas.addEventListener('mousedown', handleMouseDown, false);
+}

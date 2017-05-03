af61fd64eef78bb7be9361383e6abb0745fac4b8
ARQtty <mr.bookmix@gmail.com>
2017-05-03 15:35:08 +0300

    Replace moveViewport methods to html file

--- client/client.js
+++ client/client.js
@@ -1,67 +1,8 @@
-function send() {
-	console.log('Hello');
-}
-
 function toggleSelectPanel() {
 	var selectPanel = document.getElementById('selectPanel');
 	selectPanel.style.display = (selectPanel.style.display == 'block') ? 'none' : 'block';
 }
 
-function mvViewportUp() {
-	var y1 = window.app.graphics.y1;
-	if (y1 - (app.graphics.cellsInColumn / 2) > 0){
-		window.app.graphics.y1 = y1 - (window.app.graphics.cellsInColumn / 2);
-		window.app.graphics.y2 = window.app.graphics.y2 - (window.app.graphics.cellsInColumn / 2);
-		console.log('moveUp');
-	}else{
-		console.log('Can\'t move up')
-		window.app.graphics.y1 = 0;
-		window.app.graphics.y2 = window.app.graphics.cellsInColumn;
-	}
-	window.app.graphics.fillMap();
-}
-
-function mvViewportR() {
-	var x2 = window.app.graphics.x2;
-	if (x2 + (window.app.graphics.cellsInRow / 2) < window.app.environment.map.sizeX){
-		window.app.graphics.x1 = window.app.graphics.x1 + (window.app.graphics.cellsInRow / 2);
-		window.app.graphics.x2 = x2 + (window.app.graphics.cellsInRow / 2);
-		console.log('moveRight');
-	}else{
-		window.app.graphics.x1 = window.app.environment.map.sizeX - window.app.graphics.cellsInRow;
-		window.app.graphics.x2 = window.app.environment.map.sizeX;
-		console.log('Can\'t move right');
-	}
-	window.app.graphics.fillMap();
-}
-
-function mvViewportDown() {
-	var y2 = window.app.graphics.y2;
-	if (y2 + (window.app.graphics.cellsInColumn / 2) < window.app.environment.map.sizeY){
-		window.app.graphics.y2 = y2 + (window.app.graphics.cellsInColumn / 2);
-		window.app.graphics.y1 = window.app.graphics.y1 + (window.app.graphics.cellsInColumn / 2);
-		console.log('moveDown');
-	}else{
-		window.app.graphics.y2 = window.app.environment.map.sizeY;
-		window.app.graphics.y1 = window.app.environment.map.sizeY - window.app.graphics.cellsInColumn;
-		console.log('Can\'t move down')
-	}
-	window.app.graphics.fillMap();
-}
-
-function mvViewportL() {
-	var x1 = window.app.graphics.x1;
-	if (x1 - (window.app.graphics.cellsInRow / 2) > 0){
-		window.app.graphics.x1 = x1 - (window.app.graphics.cellsInRow / 2);
-		window.app.graphics.x2 = window.app.graphics.x2 - (window.app.graphics.cellsInRow / 2);
-		console.log('modeLeft');
-	}else{
-		window.app.graphics.x1 = 0;
-		window.app.graphics.x2 = window.app.graphics.cellsInRow;
-		console.log('Can\'t move left');
-	}
-	window.app.graphics.fillMap();
-}
 
 window.onload = function() {
 
@@ -74,6 +15,28 @@ window.onload = function() {
 		console.log('cells'+topIndexStr+leftIndexStr+' value='+window.app.graphics.cells[topIndex][leftIndex])
 
 		return window.app.graphics.cells[topIndex][leftIndex]
+	}
+
+	function getCellCoords(x, y){
+		var xIndex = Math.floor(x / cellSize)
+		var yIndex = Math.floor(y / cellSize)
+		return [xIndex, yIndex]
+	}
+
+	function fillCellWithTexture(x, y) {
+		var cellUnderCursor = getCellByPosition(x, y)	
+	
+		var cellCoord = getCellCoords(x, y)
+		var cellX = cellCoord[0]
+		var cellY = cellCoord[1]
+		
+		console.log('Поставил дом на ', cellCoord)
+		
+		cells[cellY][cellX] = newBuilding;
+		fillMap(cells)
+		var pattern = context.createPattern(house, 'repeat');
+		context.fillStyle = pattern;
+		context.fillRect(cellX * cellHeight, cellY * cellWidth, cellHeight, cellWidth);
 	}
 
 

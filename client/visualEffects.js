49825701b9e40233d9670f6cefa8ed6de95070df
ARQtty <mr.bookmix@gmail.com>
2017-08-06 13:48:33 +0300

    Add damage effect

--- client/visualEffects.js
+++ client/visualEffects.js
@@ -13,13 +13,24 @@ window.app.visualEffects = {
    hurt: function(ctx, x, y, duration, texture){
       let xBiasKoef = 0.7;
       let yBiasKoef = 0.4;
-
       let bias = cos(duration/2) * e(0.08*duration);
 
       ctx.drawImage(texture, 
                     x + bias*xBiasKoef, 
                     y + bias*yBiasKoef, 
                     32, 32);
+      //ctx.stroke()
+   },
 
-   }
+   damage: function(ctx, x, y, duration, text){
+         ctx.font = "24px fantasy";
+         ctx.strokeStyle = "black";
+         ctx.fillStyle = "red";
+         y += 16; // align center
+         duration = 34 - duration; // Inverse duration value
+         var logBias = 7*Math.log(duration);
+
+         ctx.strokeText(text, x + logBias,y - duration);
+         ctx.fillText(text, x + logBias,  y - duration);
+      }
 }})
